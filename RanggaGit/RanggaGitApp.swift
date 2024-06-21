8b06139428c0e65e480f39a0133f2cebdd2bba9e
ranggaupnvj <2310501014@mahasiswa.upnvj.ac.id>
2024-06-21 10:44:42 +0700

    Initial Commit

---
+++ RanggaGit/RanggaGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  RanggaGitApp.swift
+//  RanggaGit
+//
+//  Created by Rangga Biner on 21/06/24.
+//
+
+import SwiftUI
+
+@main
+struct RanggaGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
